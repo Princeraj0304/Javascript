4d3ce22d2ef498c4dbc0e4945714e6db19aab1b8
Prince Raj <priinceraj5403@gmail.com>
2024-12-04 22:16:05 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,18 @@
+let a= 45
+a=40
+console.log(a)
+
+
+var b=34
+var b=30
+b=2
+console.log(b)
+
+
+const c=45
+console.log(c)
+
+let num1 = 10
+let num2 = 90
+
+console.log("The addition of two numbers are ", num1 + num2)
