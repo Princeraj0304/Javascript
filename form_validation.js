8dcc0ea3af68dcdd0ead14ded2fd62a0f41cb814
Princeraj0304 <priinceraj5403@gmail.com>
2024-12-19 23:54:02 +0530

    commit

---
+++ form_validation.js
@@ -0,0 +1,20 @@
+function fun()
+{
+let a=document.getElementById('name').value
+let b=document.getElementById('contact').value
+
+if(a==""){
+    alert("Name cannot be empty")
+    document.getElementById('#name').focus()
+    return false
+}
+else if(b==""){
+      alert("Please enter your contact number")
+      document.getElementById('contact').focus()
+      return false
+
+}
+
+}
+
+
