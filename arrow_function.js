f26649a575af0e819e4b225e75ff6385d36bc13d
Princeraj0304 <priinceraj5403@gmail.com>
2024-12-25 22:14:09 +0530

    commit

---
+++ arrow_function.js
@@ -0,0 +1,33 @@
+// let a= () => {return 'prince'}
+// console.log(a())
+
+// c=30
+// let b= (a) => {return a+a}
+// console.log(b(c))
+
+
+// let a=() => {return 'Hello'}
+
+// console.log(a())
+
+// console.log((x => x*x)(5))
+
+
+// let a= function hello(){
+//     console.log("Hello")
+
+// }
+
+// a()
+
+let a= () => "hello world"
+
+console.log(a())
+
+console.log((() => "Hello Bro")());
+
+
+
+let b=(a,b) => ( a*b)
+
+console.log(b(3,4))
