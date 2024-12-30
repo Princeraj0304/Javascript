a416e9e0e7e1c94693634bea7299a47f1a78b637
Princeraj0304 <priinceraj5403@gmail.com>
2024-12-30 19:40:04 +0530

    commit

--- slice.js
+++ slice.js
@@ -1,3 +1,10 @@
 let ar=[1,2,3,4,5,6,7]
 
-console.log(ar.slice(1,4))
+// console.log(ar.slice(1,4))
+
+ar.splice(1,4,"prince")
+
+console.log(ar);
+
+// splice operator takes the first indexing and removes Element upto the specified number, It is also used to add elements in the array.
+ 
