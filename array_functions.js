d7f57c0fc5919ff16d353761336746c7002b9282
Princeraj0304 <priinceraj5403@gmail.com>
2024-12-31 01:03:22 +0530

    commit

---
+++ array_functions.js
@@ -0,0 +1,8 @@
+let arr=["🤧","😏","😒","🥲","😍","😘","😐"]
+let crr=[" sneezing Face : "," smirking Face : "," Face with tear of joy: "," Sad Face: "," Heart Eye Face: "," Face Blowing a Kiss Face :"," Neutral Face:"]
+
+
+for(let i=0;i<7;i++){
+   console.log(crr[i]+arr[i])
+}
+
