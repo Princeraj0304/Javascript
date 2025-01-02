d8737e0596ee8efa0b409459da4d6047df1477a6
Princeraj0304 <priinceraj5403@gmail.com>
2025-01-02 19:41:21 +0530

    commit

---
+++ object5.js
@@ -0,0 +1,13 @@
+let obj1={
+    name : "prince",
+    demo : function(){
+        alert("Inside Object")
+    } ,
+
+    games : ["Valorant","COD","Fortnite"]
+}
+
+
+obj1.demo()
+
+console.log(obj1.games[0]);
