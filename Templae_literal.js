116c2f48d4ba7eea2e17b41e3f1d3ef5afb2342d
Princeraj0304 <priinceraj5403@gmail.com>
2025-01-07 19:41:44 +0530

    commit

--- Templae_literal.js
+++ Templae_literal.js
@@ -4,3 +4,15 @@ let b=`Hello My
 name is Prince ${a}`
 
 console.log(b)
+
+// Template literls are denoted by backtick(` `). It is used to embed any value inside it. It is anologous to the pre tag in html.
+
+let fname="Prince "
+let mname ="Raj "
+let lname ="Sharma"
+
+let name=fname.concat(mname).concat(lname) //concat functin 
+console.log(name)
+
+
+
