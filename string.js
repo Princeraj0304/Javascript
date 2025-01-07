39ee344b8253e14ea2875c5e4c45cf9e3a817fc5
Princeraj0304 <priinceraj5403@gmail.com>
2025-01-07 19:23:28 +0530

    commit

---
+++ string.js
@@ -0,0 +1,14 @@
+// In javascript string and characters are same
+
+let str="Prince"
+
+console.log(str);
+
+console.log(str[1])
+
+console.log(str.toUpperCase())
+
+console.log(str.toLowerCase())
+
+let ct=str.repeat(3)
+console.log(ct)
