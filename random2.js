46413e80406b16d15be3d4230c47d7c1278f6097
Princeraj0304 <priinceraj5403@gmail.com>
2025-01-09 19:39:52 +0530

    commit

--- random2.js
+++ random2.js
@@ -1,12 +1,12 @@
 function gen1(){
 
-    let num=Math.trunc(Math.random()*205+1)
+    let num=Math.trunc(Math.random()*100+1)
     document.getElementById('num1').innerHTML=num
 
-    let num3=Math.trunc(Math.random()*10+1)
+    let num3=Math.trunc(Math.random()*200+1)
     document.getElementById('num3').innerHTML=num3
-    
-    let num2=Math.trunc(Math.random()*15+1)
+
+    let num2=Math.trunc(Math.random()*30+1)
     document.getElementById('num2').innerHTML=num2
 
 }
