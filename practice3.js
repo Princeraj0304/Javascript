df0287c2b5190385ca01d0d1a177b000a202142a
Princeraj0304 <priinceraj5403@gmail.com>
2025-01-10 18:28:02 +0530

    commit

---
+++ practice3.js
@@ -0,0 +1,14 @@
+let arr=[
+    {
+        name :"Prince",
+        age :20
+    },
+    {
+        name : "Rahil",
+        age : 20
+    }
+]
+
+let names=arr.map((e) => e.name )
+
+console.log(names);
