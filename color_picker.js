4560cf362bfc84f7b50315db611aeab0a2e01e74
Princeraj0304 <priinceraj5403@gmail.com>
2025-01-15 19:56:33 +0530

    commit

--- color_picker.js
+++ color_picker.js
@@ -1,16 +1,18 @@
 let x=[1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
-
+let color=document.getElementById('color')
 // const change=() =>{
 //    
 // }
 
+setInterval(
 function change(){
     let y= '#'
     for(let i=0;i<6;i++){
         y=y+x[random_gen()]
     }
-    alert(y)
+    document.body.style.backgroundColor=y
 }
+,100)
 
 const random_gen=()=>{
      return Math.floor((Math.random() * x.length))
