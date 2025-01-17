f2f9252011e57564d2fc0b3421af70d2d488174a
Princeraj0304 <priinceraj5403@gmail.com>
2025-01-17 19:12:00 +0530

    commit

--- color_picker.js
+++ color_picker.js
@@ -4,7 +4,7 @@ let color=document.getElementById('color')
 //    
 // }
 
-setInterval(
+let z=setInterval(
 function change(){
     let y= '#'
     for(let i=0;i<6;i++){
@@ -14,6 +14,12 @@ function change(){
 }
 ,100)
 
+setTimeout(z,5000)
+
+clearInterval()
 const random_gen=()=>{
      return Math.floor((Math.random() * x.length))
 }
+
+// object array spread-operator ternary-operator exception-handling local-storage Array-to-string String-to-array
+// Array-methods
