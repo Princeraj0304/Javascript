40a13c30033914898b075c64341ba0de2c42512f
Princeraj0304 <priinceraj5403@gmail.com>
2025-01-26 22:38:34 +0530

    commit

---
+++ signup2.js
@@ -0,0 +1,9 @@
+function auth(){
+    let Signup ={
+        name : document.querySelector('#name').value,
+        email :document.querySelector('#email').value,
+        password : document.querySelector('#pass').value
+
+    }
+    localStorage.setItem('')
+}
