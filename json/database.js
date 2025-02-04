5865a52e47a5511472014d3ab1357bafd8a339ed
Princeraj0304 <priinceraj5403@gmail.com>
2025-02-04 19:53:03 +0530

    commit

--- json/database.js
+++ json/database.js
@@ -1,8 +1,20 @@
 async function getdata(){
     let data= await fetch('http://localhost:3000/student')
     let res = await data.json()
-    console.log(res)
+
+let b=a.map((e)=>`
+<tr>
+<td>${e.name}</td> 
+<td>${e.age}</td> 
+<td>${e.city}</td> 
+<td>${e.contact}</td> 
+</tr>
+`).join('')
+
+document.getElementById('table').innerHTML=b
+
 }
 
-
 getdata()
+
+
