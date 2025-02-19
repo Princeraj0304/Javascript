c1cb420f24931ecc78ef5e849090b7cb4ea6045e
Princeraj0304 <priinceraj5403@gmail.com>
2025-02-19 21:16:06 +0530

    commit

--- destructuring.js
+++ destructuring.js
@@ -3,7 +3,7 @@ let [name, age , gender] = ["Prince", 20, "Male"];
 console.log(name);
 
 
-let ar=["prince Raj",20,"Bhopal"]
+let ar=["prince",20,"Bhopal"]
 ar.unshift("code","hello")
 console.log(ar)
 
